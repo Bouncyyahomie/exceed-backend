e74df2242fef2775e196da95d55346fed7bdb8d4
Bouncyyahomie <thornny.s@gmail.com>
2021-02-18 18:40:18 +0700

    counter washing

--- backend_exceed.py
+++ backend_exceed.py
@@ -8,5 +8,24 @@ mongo = PyMongo(app)
 melodyCollection = mongo.db.melody
 countingCollection = mongo.db.counting
 
+
+@app.route('/count' , methods=['GET'])
+def get_count():
+    query = countingCollection.find_one_or_404()
+    count = {'count': query['count']}
+    return count.values()
+
+@app.route('/counter', methods=['PATCH'])
+def counter():
+    data = request.json
+    filt = {'type': 'washinghand'}
+    getter = get_count()
+    counter = getter["count"] + 1
+    counting = {"$set":{'count':counter}}
+    countingCollection.update_one(filt,counting)
+
+    return {'result': 'Someone is washing hand'}
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='3000', debug=True)
